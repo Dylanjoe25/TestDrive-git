ec53cb0126c087372db0a70a41fa9d532f18225d
dylanjoe25 <cliffordpumulo@gmail.com>
2022-04-25 17:45:45 +0200

    Modified added Another

--- Administrator/bin/Debug/AllUsers.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class AllUsers
-
-End Class
---
+++ ExamApp/exam.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OpenExam
+{
+	public class OpenBook
+	{
+
+
+		int count, total = 2;
+
+
+		//string answer2;
+		//string answer3;
+
+
+
+
+
+
+		public void ScienceQ()
+		{
+			Console.WriteLine("\t\t1.\tWhat is the unit of power?");
+			Console.WriteLine("\t\tA.\tdecibel");
+			Console.WriteLine("\t\tB.\tpower");
+			Console.WriteLine("\t\tC.\twatt");
+			Console.WriteLine("\t\tD.\tamps");
+
+			Console.Write("\t\t");
+			char answer1 = Convert.ToChar(Console.ReadLine());
+			
+			Console.WriteLine("\n");
+
+			Console.WriteLine("\t\t2.\tWhat is the capital city of Kuwait?");
+			Console.WriteLine("\t\tA.\tIstambul");
+			Console.WriteLine("\t\tB.\tTripoli");
+			Console.WriteLine("\t\tC.\tKuwait City");
+			Console.WriteLine("\t\tD.\tCairo");
+			Console.Write("\t\t");
+			char answer2 = Convert.ToChar(Console.ReadLine());
+			
+			Console.WriteLine("\n");
+
+
+
+			if (answer1 == 'c' || answer1 == 'C')
+			{
+				//Console.WriteLine("!\t\tNumber 1 your answer {0} is correct", answer1);
+				count += 1;
+			}
+			else
+			{
+				Console.WriteLine("\t\tNumber 1 your answer {0} is wrong", answer1);
+			}
+
+			if (answer2 == 'c' || answer2 == 'C')
+			{
+				//Console.WriteLine("\t\tNumber 2 your answer {0} is correct", answer2);
+				count += 1;
+			}
+			else
+			{
+				Console.WriteLine("\t\tNumber 2 your answer {0} is wrong", answer2);
+			}
+			Console.WriteLine("\n");
+
+			Console.Beep();
+			Console.WriteLine("\t\tYou got {0} out of {1}", count, total);
+
+
+			if (count == total)
+			{
+				Console.WriteLine("\t\tExcellent Work!!");
+			}
+			else if (count == 0)
+			{
+				Console.WriteLine("\t\tFail");
+			}
+			else
+			{
+				Console.WriteLine("!\t\tFair");
+			}
+		}
+	}
+
+	class Exam
+	{
+		static void Main(string[] args)
+		{
+
+			OpenBook o = new OpenBook();
+
+			o.ScienceQ();
+
+
+
+			Console.ReadLine();
+		}
+	}
+}
